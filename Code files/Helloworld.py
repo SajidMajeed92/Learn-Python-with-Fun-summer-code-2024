e8a9ea1778a8cfee960af7c95784c550890bd86b
sajidmajeed92 <sajidmajeed.ist@gmail.com>
2024-06-06 23:46:02 +0500

    Week1_class2

--- Code files/Helloworld.py
+++ Code files/Helloworld.py
@@ -1,1 +1,24 @@
 print("learn python with fun at NED")
+print(2)
+print(2+2)
+print(100-50)
+print(10*5)
+print(10/2)
+num1 =10
+print(num1)
+num2=5
+print(num2)
+print(type(num2))
+print(num1+num2)
+name ="Sajid Majeed"
+print(name)
+print(type(name))
+
+name = "your name"
+fathername = "your's Father Name"
+Age = 23
+gender = 'gender'
+class = 'class name'
+school name= "school name"
+
+
